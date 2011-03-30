308217b2db2031184a29ace985b8a2bf8e0a2c03
hiratara <hiratara@cpan.org>
2011-03-30 20:19:29 +0900

    Initial commit.

---
+++ register.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Cwd qw/cwd/;
+use File::Basename qw/dirname/;
+use LWP::Simple qw/mirror/;
+
+my $git_exe = '/usr/bin/git';
+my $data_dir = (dirname __FILE__) . "/data";
+my $base_url = 'http://www.bizoole.com/power';
+
+my @files = qw/all.all timetable.txt runtable.txt/;
+
+sub exec_git(@) {
+    my @cmd = @_;
+
+    my $orig_dir = cwd;
+    chdir $data_dir or die $!;
+
+    my $exit_code = system $git_exe, @cmd;
+    $exit_code >> 8 == 0 or die +(join ' ', @cmd) . " failed: $exit_code";
+
+    chdir $orig_dir or die $!;
+}
+
+exec_git "clean", "-fd";
+
+for (@files) {
+    my $code = mirror "$base_url/$_", "$data_dir/$_";
+    $code =~ /^[23]/ or die "bad status code: $code";
+}
+
+exec_git "add", $_ for @files;
+exec_git "commit", "-m", "the latest files.";
+exec_git "push", "origin", "master";
